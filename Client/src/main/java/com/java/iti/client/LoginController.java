75f18c5659d3b99dbede8ed855189d9353ea7981
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-01-26 23:21:02 +0200

    redesigned login scene, added validation for phone number and password fields

--- Client/src/main/java/com/java/iti/client/LoginController.java
+++ Client/src/main/java/com/java/iti/client/LoginController.java
@@ -1,32 +1,65 @@
 package com.java.iti.client;
 
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.NumberValidator;
+import com.jfoenix.validation.RequiredFieldValidator;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
 
     @FXML
-    private TextField passwordField;
+    private JFXTextField phoneNumberField;
 
     @FXML
-    private TextField phoneNumberField;
+    private JFXTextField passwordField;
 
     @FXML
     private Button signInBtn;
 
-    @FXML
-    private Button signUpBtn;
 
     @FXML
-    void handleSigninButton(MouseEvent event) {
+    void handleSignIn(MouseEvent event) {
 
     }
 
     @FXML
-    void handleSignupButton(MouseEvent event) {
+    void handleSignUp(MouseEvent event) {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //RequiredField
+        RequiredFieldValidator requiredField = new RequiredFieldValidator();
+        requiredField.setMessage("Required Field");
+        phoneNumberField.getValidators().add(requiredField);
+        phoneNumberField.focusedProperty().addListener((o, oldval, newval) -> {
+            if (!newval) phoneNumberField.validate();
+        });
+        passwordField.getValidators().add(requiredField);
+        passwordField.focusedProperty().addListener((o, oldval, newval) -> {
+            if (!newval) passwordField.validate();
+            requiredField.setIcon(new ImageView(new Image(getClass().getResourceAsStream("/images/error.png"))));
+        });
+
+
+        //NumberField Only
+        NumberValidator requiredNumber = new NumberValidator();
+        requiredNumber.setMessage("enter a proper phone number");
+        phoneNumberField.getValidators().add(requiredNumber);
+        phoneNumberField.focusedProperty().addListener((o, oldval, newval) -> {
+            if (!newval) phoneNumberField.validate();
+            requiredNumber.setIcon(new ImageView(new Image(getClass().getResourceAsStream("/images/error.png"))));
+        });
+
+    }
 }
--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.java.iti.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
 
 
     opens com.java.iti.client to javafx.fxml;

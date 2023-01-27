7606b5b7e7efc053de26e8d3e7fe7276608cd46d
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-01-27 03:56:22 +0200

    added more convenient validation for phone number field, now accepting only egyptian phone numbers starting with 010,011,012,015

--- Client/src/main/java/com/java/iti/client/LoginController.java
+++ Client/src/main/java/com/java/iti/client/LoginController.java
@@ -1,19 +1,29 @@
 package com.java.iti.client;
 
+import com.java.iti.client.utils.PhoneNumberValidator;
 import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.validation.NumberValidator;
 import com.jfoenix.validation.RequiredFieldValidator;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
+    private AnchorPane signInPane;
 
     @FXML
     private JFXTextField phoneNumberField;
@@ -28,7 +38,9 @@ public class LoginController implements Initializable {
     @FXML
     void handleSignIn(MouseEvent event) {
 
+
     }
+
 
     @FXML
     void handleSignUp(MouseEvent event) {
@@ -38,28 +50,32 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        //RequiredField
-        RequiredFieldValidator requiredField = new RequiredFieldValidator();
-        requiredField.setMessage("Required Field");
-        phoneNumberField.getValidators().add(requiredField);
-        phoneNumberField.focusedProperty().addListener((o, oldval, newval) -> {
-            if (!newval) phoneNumberField.validate();
-        });
-        passwordField.getValidators().add(requiredField);
-        passwordField.focusedProperty().addListener((o, oldval, newval) -> {
-            if (!newval) passwordField.validate();
-            requiredField.setIcon(new ImageView(new Image(getClass().getResourceAsStream("/images/error.png"))));
-        });
+    }
 
+    private boolean validateFields() {
+        boolean validationFlag = false;
 
-        //NumberField Only
-        NumberValidator requiredNumber = new NumberValidator();
-        requiredNumber.setMessage("enter a proper phone number");
+        //Required Field Validation
+        RequiredFieldValidator requiredNumber = new RequiredFieldValidator();
+        RequiredFieldValidator requiredPassword = new RequiredFieldValidator();
+        requiredNumber.setMessage("\t\t\t\t\t\t\t\t     can't be empty!");
+        requiredPassword.setMessage("\t\t\t\t\t\t\t\t     can't be empty!");
         phoneNumberField.getValidators().add(requiredNumber);
-        phoneNumberField.focusedProperty().addListener((o, oldval, newval) -> {
-            if (!newval) phoneNumberField.validate();
+        passwordField.getValidators().add(requiredPassword);
+        if (!phoneNumberField.validate())
             requiredNumber.setIcon(new ImageView(new Image(getClass().getResourceAsStream("/images/error.png"))));
-        });
+        if (!passwordField.validate())
+            requiredPassword.setIcon(new ImageView(new Image(getClass().getResourceAsStream("/images/error.png"))));
+        else validationFlag = true;
 
+        //Phone Number Validation
+        PhoneNumberValidator validNumber = new PhoneNumberValidator();
+        validNumber.setMessage("\t\t\t\t\t\t\t       enter valid number");
+        phoneNumberField.getValidators().add(validNumber);
+        if (!phoneNumberField.validate()) {
+            validNumber.setIcon(new ImageView(new Image(getClass().getResourceAsStream("/images/error.png"))));
+        } else validationFlag = true;
+
+        return validationFlag;
     }
 }

4d84b977ba1bb6645ceb5fbc161b9e37982a124d
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-01-27 04:21:35 +0200

    adding handler for signUp button .. transition to sign up page

--- Client/src/main/java/com/java/iti/client/LoginController.java
+++ Client/src/main/java/com/java/iti/client/LoginController.java
@@ -3,27 +3,34 @@ package com.java.iti.client;
 import com.java.iti.client.utils.PhoneNumberValidator;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.validation.RequiredFieldValidator;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
     @FXML
-    private StackPane stackPane;
+    private StackPane parentContainer;
 
     @FXML
-    private AnchorPane signInPane;
+    private AnchorPane anchorRoot;
 
     @FXML
     private JFXTextField phoneNumberField;
@@ -37,14 +44,40 @@ public class LoginController implements Initializable {
 
     @FXML
     void handleSignIn(MouseEvent event) {
-
-
+        if (validateFields()) {
+            /*
+            //Transition
+            // To
+            // User
+            // Home Screen chats
+            // Here
+             */
+            System.out.println("Valid");
+        } else System.out.println("not valid");
     }
 
 
     @FXML
     void handleSignUp(MouseEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/FXML/sign-up.fxml"));
+            Scene scene = signInBtn.getScene();
+            root.translateXProperty().set(scene.getWidth());
 
+            parentContainer.getChildren().add(root);
+
+            Timeline timeline = new Timeline();
+            KeyValue kv = new KeyValue(root.translateXProperty(), 0, Interpolator.EASE_BOTH);
+            KeyFrame kf = new KeyFrame(Duration.seconds(.7), kv);
+            timeline.getKeyFrames().add(kf);
+            timeline.setOnFinished(t -> {
+                parentContainer.getChildren().remove(anchorRoot);
+            });
+            timeline.play();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override

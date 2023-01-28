70ac4e402283b849f93cac52605db206985aec25
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-01-28 06:44:13 +0200

    creating and styling sign-up page3, added custom design for date picker fixed some scene loading bugs

---
+++ Client/src/main/java/com/java/iti/client/controllers/SignUp3Controller.java
@@ -0,0 +1,25 @@
+package com.java.iti.client.controllers;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SignUp3Controller {
+
+    @FXML
+    private AnchorPane signInPane;
+
+    @FXML
+    private JFXTextField displayNameField;
+
+    @FXML
+    private Button confirmCreateAccountBtn;
+
+    @FXML
+    void handleConfirmCreateAccount(MouseEvent event) {
+
+    }
+
+}
--- Client/src/main/java/com/java/iti/client/controllers/SignUpController.java
+++ Client/src/main/java/com/java/iti/client/controllers/SignUpController.java
@@ -4,12 +4,21 @@ import com.java.iti.client.utils.PasswordValidator;
 import com.java.iti.client.utils.PhoneNumberValidator;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.validation.RequiredFieldValidator;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,18 +38,36 @@ public class SignUpController implements Initializable {
 
     @FXML
     private Button signUpNextBtn1;
+//    private Parent signUpPane2;
 
     @FXML
-    void handleNextBtn(MouseEvent event) {
-        if (validateFields())
-            System.out.println("valid");
-        else System.out.println("invalid");
+    void handleNextBtn(MouseEvent event) throws IOException {
+        if (validateFields()) {
+            Parent signUpPane2 = FXMLLoader.load(getClass().getResource("/FXML/sign-up-2.fxml"));
+            Scene currentScene = signUpNextBtn1.getScene();
+            signUpPane2.translateXProperty().set(currentScene.getWidth());
+
+            signInPane.getChildren().add(signUpPane2);
+
+            Timeline timeline = new Timeline();
+            KeyValue kv = new KeyValue(signUpPane2.translateXProperty(), 0, Interpolator.EASE_BOTH);
+            KeyFrame kf = new KeyFrame(Duration.seconds(.7), kv);
+            timeline.getKeyFrames().add(kf);
+//            timeline.setOnFinished(t -> {
+//                LoginController.s.getChildren().remove(signUpPane2);
+//            });
+            timeline.play();
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 //        try {
-//            Parent signUpPane2 = FXMLLoader.load(getClass().getResource("/FXML/sign-up-2.fxml"));
+//        try {
+//            signUpPane2 = FXMLLoader.load(getClass().getResource("/FXML/sign-up-2.fxml"));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 //            Parent signUpPane3 = FXMLLoader.load(getClass().getResource("/FXML/sign-up-3.fxml"));
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);

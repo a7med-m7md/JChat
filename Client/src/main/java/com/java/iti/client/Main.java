2e555bf776527d4ff1764c9fe696ee1c8e72521f
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-01-28 16:44:26 +0200

    styling updates

--- Client/src/main/java/com/java/iti/client/Main.java
+++ Client/src/main/java/com/java/iti/client/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/FXML/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("JChat");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }

bda14606c4f0ec02235bb9cf6a116856f2fcb4bb
Ahmed Mohamed <new00hurt@gmail.com>
2023-01-28 03:26:59 +0200

    DB connection with prop file + Add Group Dao and Group member Dao

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.java.iti.client {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.java.iti.client to javafx.fxml;
     exports com.java.iti.client;

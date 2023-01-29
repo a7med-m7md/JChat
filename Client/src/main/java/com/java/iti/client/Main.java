d02c6f47f786dea70e904acbe9645d4c9333e418
aa800 <aa800291@gmail.com>
2023-01-30 00:40:01 +0200

    -Add UserDao with several user funcationalities.

--- Client/src/main/java/com/java/iti/client/Main.java
+++ Client/src/main/java/com/java/iti/client/Main.java
@@ -1,5 +1,9 @@
 package com.java.iti.client;
 
+import com.java.iti.client.model.user.Gender;
+import com.java.iti.client.model.user.User;
+import com.java.iti.client.model.user.UserStatus;
+import com.java.iti.client.repository.userDao.UserDao;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -14,6 +18,21 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/FXML/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("JChat");
+        User user = new User(
+                "011014",
+                "mohamed mahmoud",
+                "mm22@gmail",
+                "picture str path",
+                "1234password",
+                Gender.MALE,
+                "egypt",
+                "10sept",
+                "software engineer",
+                UserStatus.AVAILABLE
+        );
+        UserDao userDao = new UserDao();
+        User res = userDao.save(user);
+        System.out.println(res.getStatus().name());
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
         stage.setResizable(false);
         stage.setScene(scene);
@@ -22,5 +41,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
+
     }
 }

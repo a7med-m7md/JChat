68eaf0abcde3653d882ff4fedb567c23ee8cc05e
Ahmed Mohamed <new00hurt@gmail.com>
2023-02-03 16:07:53 +0200

    Test client rmi connection

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -3,10 +3,12 @@ module com.java.iti.client {
     requires javafx.fxml;
     requires java.sql;
     requires com.jfoenix;
+    requires java.rmi;
 
 
-    opens com.java.iti.client to javafx.fxml;
-    exports com.java.iti.client;
-    exports com.java.iti.client.controllers;
-    opens com.java.iti.client.controllers to javafx.fxml;
+    opens com.java.iti to javafx.fxml;
+    exports com.java.iti;
+    exports com.java.iti.controllers;
+    exports com.java.iti.utils.interfaces;
+    opens com.java.iti.controllers to javafx.fxml;
 }

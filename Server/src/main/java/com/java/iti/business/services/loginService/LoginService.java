f4164344ca3085e0a007ffbea1688abf7610377b
ShaimaaHassan3 <70854824+ShaimaaHassan3@users.noreply.github.com>
2023-02-04 14:18:39 +0200

    update LoginServices

--- Server/src/main/java/com/java/iti/business/services/loginService/LoginService.java
+++ Server/src/main/java/com/java/iti/business/services/loginService/LoginService.java
@@ -1,9 +1,10 @@
 package com.java.iti.business.services.loginService;
 
 import com.java.iti.business.dtos.UserDto;
+import com.java.iti.network.interfaces.LoginEntity;
 
 public interface LoginService {
-    public boolean logInto(UserDto dto);
+    public boolean logInto(LoginEntity dto);
 
     public void logOut(int id);
 }
--- Server/src/main/java/com/java/iti/business/services/loginService/LoginServiceImp.java
+++ Server/src/main/java/com/java/iti/business/services/loginService/LoginServiceImp.java
@@ -1,6 +1,8 @@
 package com.java.iti.business.services.loginService;
+
 import com.java.iti.business.dtos.UserDto;
 import com.java.iti.exception.CredentialException;
+import com.java.iti.network.interfaces.LoginEntity;
 import com.java.iti.persistance.entities.UserEntity;
 import com.java.iti.persistance.dao.UserDao;
 
@@ -9,23 +11,27 @@ import java.util.Optional;
 
 public class LoginServiceImp implements LoginService {
     UserDao userDao = new UserDao();
+
     @Override
-    public boolean logInto(UserDto userDto) throws CredentialException {
-        Optional<UserEntity> currentUser = userDao.findById((int) userDto.getId());
+    public boolean logInto(LoginEntity userDto) throws CredentialException {
+        Optional<UserEntity> currentUser = userDao.findById(1);
         if (currentUser == null) {
             throw new CredentialException("Phone Or Password May Be Invalid");
         }
-        if(userDto.getStatus().equals("AVAILABLE")) {
-            throw new CredentialException("User is already logged in");
-        }
-        if(!currentUser.get().getPassword().equals(userDto.getPassword())) {
+        if (!currentUser.get().getPassword().equals(userDto.getPassword()) &&
+                !currentUser.get().getMobile().equals(userDto.getMobile())) {
             throw new CredentialException("Invalid password");
         }
+        userDao.userLogin(userDto);
         return true;
     }
 
     @Override
     public void logOut(int id) {
+        Optional<UserEntity> currentUser = userDao.findById(1);
+        if (currentUser == null) {
+            throw new CredentialException("Phone Or Password May Be Invalid");
+        }
         userDao.delete(id);
     }
 }

8eba4ff5ec10cf5d1fbd1cf3f870c45c743a7daf
adelahmedd <32197710+aadelaahmed@users.noreply.github.com>
2023-02-10 19:06:04 +0200

    set logic for transfering file in server

--- Server/src/main/java/Server/network/FileTransferHandled.java
+++ Server/src/main/java/Server/network/FileTransferHandled.java
@@ -33,7 +33,41 @@ public class FileTransferHandled implements Runnable{
 
     @Override
     public void run() {
+        while (true) {
 
+            try {
+                // Wait for a client to connect and when they do create a socket to communicate with them.
+                // Stream to receive data from the client through the socket.
+                DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+                // Read the size of the file name so know when to stop reading.
+                int fileNameLength = dataInputStream.readInt();
+                // If the file exists
+                if (fileNameLength > 0) {
+                    // Byte array to hold name of file.
+                    byte[] fileNameBytes = new byte[fileNameLength];
+                    // Read from the input stream into the byte array.
+                    dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+                    // Create the file name from the byte array.
+                    String fileName = new String(fileNameBytes);
+                    // Read how much data to expect for the actual content of the file.
+                    int fileContentLength = dataInputStream.readInt();
+                    // If the file exists.
+                    if (fileContentLength > 0) {
+                        // Array to hold the file data.
+                        byte[] fileContentBytes = new byte[fileContentLength];
+                        // Read from the input stream into the fileContentBytes array.
+                        dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
+                        //TODO -> then we need to create logic here which is used to store file in server or db then send it to the receiver client
+                        File fileToDownload = new File(fileName);
+                        FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
+                        fileOutputStream.write(fileContentBytes);
+                        fileOutputStream.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static String getFileExtension(String fileName) {

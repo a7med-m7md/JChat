ee1029b35f34fd53d76f153445f9231e969f9ce9
Mahmoud-Elsaqqa <119105101+Mahmoud-Elsaqqa@users.noreply.github.com>
2023-02-10 17:55:55 +0200

    Revert "User info UI with binding"

--- SharedUtilities/src/main/java/model/user/Gender.java
+++ SharedUtilities/src/main/java/model/user/Gender.java
@@ -1,12 +1,5 @@
 package model.user;
 
 public enum Gender {
-    MALE("Male"), FEMALE("Female");
-    private String gender;
-    Gender (String gender) {
-        this.gender = gender;
-    }
-    public String getGender() {
-        return gender;
-    }
+    MALE, FEMALE
 }

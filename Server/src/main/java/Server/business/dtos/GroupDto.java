a61756fada9a972592c777de29a6e841fd50f277
ShaimaaHassan3 <70854824+ShaimaaHassan3@users.noreply.github.com>
2023-02-10 13:04:28 +0200

    Handle Entity in Server

---
+++ Server/src/main/java/Server/business/dtos/GroupDto.java
@@ -0,0 +1,58 @@
+package Server.business.dtos;
+
+import java.time.LocalDateTime;
+
+public class GroupDto {
+
+    private long id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private long owner_id;
+
+    public GroupDto(String name, String description, long owner_id) {
+        this.name = name;
+        this.description = description;
+        this.owner_id = owner_id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(long owner_id) {
+        this.owner_id = owner_id;
+    }
+}

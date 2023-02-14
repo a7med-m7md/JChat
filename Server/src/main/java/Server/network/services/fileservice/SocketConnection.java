3b71dc4de5332a596cb5078ca87ebd41f68c5403
adelahmedd <32197710+aadelaahmed@users.noreply.github.com>
2023-02-14 11:51:48 +0200

    finish transferring file with unlimited size.

--- Server/src/main/java/Server/network/services/fileservice/SocketConnection.java
+++ Server/src/main/java/Server/network/services/fileservice/SocketConnection.java
@@ -10,11 +10,10 @@ import java.net.Socket;
 import static utils.Constants.SOCKET_SERVER_PORT;
 
 public class SocketConnection {
-    private DataOutputStream dataOutputStream = null;
-    private DataInputStream dataInputStream = null;
     Socket clientSocket;
     ServerSocket serverSocket;
     FileTransferHandled fileTransferHandled;
+
     public SocketConnection() {
         startConnection();
 
@@ -38,6 +37,8 @@ public class SocketConnection {
                                 th.start();
                             } catch (IOException e) {
                                 e.printStackTrace();
+                                closeResources();
+                                break;
                             }
                         }
                     }
@@ -47,39 +48,15 @@ public class SocketConnection {
             e.printStackTrace();
             //closeResources();
         }
-        /* new Thread(() -> {
-                while (true){
-                    try {
-                        serverSocket
-                                = new ServerSocket(SOCKET_SERVER_PORT);
-                        System.out.println(
-                                "Server is Starting in Port 1200");
-                        clientSocket = serverSocket.accept();
-                        receiveFile(clientSocket);
-                        System.out.println("Connected");
-                        dataInputStream = new DataInputStream(
-                                clientSocket.getInputStream());
-                        dataOutputStream = new DataOutputStream(
-                                clientSocket.getOutputStream());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        //closeResources();
-                    }
-                }
-            }).start();*/
     }
 
-    public void receiveFile(Socket clientSocket) {
-        Thread th = new Thread(new FileTransferHandled(clientSocket));
-        th.start();
-    }
 
     private void closeResources() {
         try {
-            this.clientSocket.close();
-            this.serverSocket.close();
-            this.dataOutputStream.close();
-            this.dataInputStream.close();
+            if (serverSocket != null)
+                this.serverSocket.close();
+            if (clientSocket != null)
+                this.clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
